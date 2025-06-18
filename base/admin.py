e0c02ed0550243ec43ede57fe71cc0aa4c306910
xjackhammered <hammerofjack7@gmail.com>
2025-06-18 17:04:14 +0600

    added all the models and configured media

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Course, Posts, User
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Course)
+admin.site.register(Posts)
--- base/models.py
+++ base/models.py
@@ -6,6 +6,21 @@ class User(AbstractUser):
     name = models.CharField(max_length=200, null=True)
     email = models.EmailField(null=True, unique=True)
     avatar = models.ImageField(null=True, default="avatar.svg")
+    type = models.CharField(null=False)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+class Course(models.Model):
+    creator = models.ManyToManyField(User)
+    name = models.CharField(max_length=200)
+    code = models.TextField(max_length=10)
+    created = models.DateTimeField(auto_now_add=True)
+
+class Posts(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    image = models.ImageField(upload_to='post_images/', blank=True, null=True)
+    file = models.FileField(upload_to="post_files/", blank=True, null=True)
+    description = models.TextField(max_length=1000)
+    created = models.DateTimeField(auto_now_add=True)
